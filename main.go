c4c27e2217448eb07bd4fbb5e9af9f29da21f96a
ronething <axingfly@gmail.com>
2020-04-12 12:32:59 +0800

    init: first commit

---
+++ main.go
@@ -0,0 +1,8 @@
+// author: ashing
+// time: 2020/4/12 12:19 下午
+// mail: axingfly@gmail.com
+// Less is more.
+
+package main
+
+
---
+++ main.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+# 简单公式计算
+import sys
+from math import sqrt
+
+
+def calc_sample(a: int, b: int):
+    c = (a * 10 ** 6) / (14.3 * 1000 * b ** 2)
+    c = format(c, ".3f")
+    print("(a * 10 ** 6) / (14.3 * 1000 * b ** 2) = ", c)
+    d = 1 - sqrt(1 - 2 * float(c))
+    d = format(d, ".3f")
+    print("1 - sqrt(1 - 2 * c) = ", d)
+    e = 1 - 0.5 * float(d)
+    e = format(e, ".3f")
+    print(" 1 - 0.5 * d = ", e)
+    f = (a * 10 ** 6) / (360 * float(e) * b)
+    f = "%.0f" % f
+    print("(a * 10 ** 6) / (360 * e * b) = ", f)
+
+
+if __name__ == "__main__":
+    # input a b
+    # calc(a=2.5, b=60)
+    try:
+        # print(sys.argv)
+        a = float(sys.argv[1])
+        b = float(sys.argv[2])
+    except:
+        print("error")
+    calc_sample(a, b)
